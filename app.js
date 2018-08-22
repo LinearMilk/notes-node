59273b87291148c2bef2254ddcd78540272f3432
LinearMilk <witold.henszel@gmail.com>
2018-08-22 16:25:14 +0200

    creating files

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("Starting app.js");
+
+const fs = require("fs");
+const os = require("os");
+const notes = require("./notes.js");
+
+var user = os.userInfo();
+
+fs.appendFileSync("greetings.txt", `"Hello ${user.username}!`);
